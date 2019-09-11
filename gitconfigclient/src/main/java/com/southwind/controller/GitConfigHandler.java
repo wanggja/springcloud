f42bfe9ad0375baed89a2ddf5cb468dfca589b72
wanggaojian <wanggaojian@hxrainbow.com>
2019-09-11 10:27:43 +0800

    git仓库配置中心的文件名更新

--- gitconfigclient/src/main/java/com/southwind/controller/GitConfigHandler.java
+++ gitconfigclient/src/main/java/com/southwind/controller/GitConfigHandler.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/git")
 public class GitConfigHandler {
 
-//    @Value("${server.port}")
-//    private String port;
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("/index")
     public String index() {
-        return "123456";
+        return this.port;
     }
 }
